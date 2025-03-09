342f4b14dba65d1b61418c7495ac172e62c37a2e
Ranvirdeshmukh <ranvir.r.deshmukh.26@dartmouth.edu>
2025-03-09 16:57:57 -0400

    added envs

--- src/pages/CourseReviewsPage/CourseReviewsPage.jsx
+++ src/pages/CourseReviewsPage/CourseReviewsPage.jsx
@@ -2347,7 +2347,7 @@ useEffect(() => {
           'educ': 'education',
           'engl': 'english-and-creative-writing',
           'engs': 'engineering-sciences',
-          'envs': 'environmental-studies',
+          'envs': 'environmental-studies-program', // Updated path for Environmental Studies
           'film': 'film-and-media-studies',
           'fren': 'french-and-italian-languages-and-literatures',
           'frit': 'french-and-italian-languages-and-literatures',
@@ -2407,6 +2407,7 @@ useEffect(() => {
           'arth',
           'grk', // Adding Greek to the departments that need /en/ in the URL
           'span', // Adding Spanish to the departments that need /en/ in the URL
+          'envs', // Adding Environmental Studies to the departments that need /en/ in the URL
           // Add more as needed
         ];
         
@@ -2527,6 +2528,13 @@ useEffect(() => {
           const portUrl = `https://dartmouth.smartcatalogiq.com/${enPath}current/orc/departments-programs-undergraduate/spanish-and-portuguese-languages-and-literatures/${deptCode}-portuguese/${deptCode}-${courseNum}/`;
           console.log(`Generated ORC link for ${courseId}: ${portUrl}`);
           return portUrl;
+        }
+        
+        // Special case for ENVS - it has a different URL structure
+        if (deptCode === 'envs') {
+          const envsUrl = `https://dartmouth.smartcatalogiq.com/${enPath}current/orc/departments-programs-undergraduate/environmental-studies-program/${deptCode}-environmental-studies/${deptCode}-${courseNum}/`;
+          console.log(`Generated ORC link for ${courseId}: ${envsUrl}`);
+          return envsUrl;
         }
         
         // Special case for GOVT - it has different URL structures based on course number ranges
