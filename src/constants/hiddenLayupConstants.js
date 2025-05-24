eda4afe4c230317280705f29817cef1fb9cd585a
Ranvirdeshmukh <ranvir.r.deshmukh.26@dartmouth.edu>
2025-05-24 17:53:30 -0400

    added

--- src/constants/hiddenLayupConstants.js
+++ src/constants/hiddenLayupConstants.js
@@ -3,7 +3,6 @@
 export const hiddenLayupCourseIds = [  'PHIL_PHIL026__Philosophy_and_Computers',
   'ENGS_ENGS011__The_Way_Things_Work__A_Visual_Introduction_to_Engineering',
   'JWST_JWST010__History_and_Culture_of_the_Jews_I__The_Classical_Period',
-  'ASCL_ASCL010_01__Introduction_to_Chinese_Culture',
   'ANTH_ANTH050_34__Peoples_of_Oceania',  // Note: Adjust if the '/courses/' prefix is part of the ID in Firestore.
   'ANTH_ANTH006__Introduction_to_Biological_Anthropology',
   'MATH_MATH030_04__Evolutionary_Game_Theory_and_Applications',
