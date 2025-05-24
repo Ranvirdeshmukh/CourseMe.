046af10b934a5297ec6938f0bcc9ea4482b633f4
Ranvirdeshmukh <ranvir.r.deshmukh.26@dartmouth.edu>
2025-05-24 17:58:09 -0400

    added

--- src/constants/hiddenLayupConstants.js
+++ src/constants/hiddenLayupConstants.js
@@ -2,7 +2,8 @@
 
 export const hiddenLayupCourseIds = [  'PHIL_PHIL026__Philosophy_and_Computers',
   'ENGS_ENGS011__The_Way_Things_Work__A_Visual_Introduction_to_Engineering',
-  'JWST_JWST010__History_and_Culture_of_the_Jews_I__The_Classical_Period',
+  'TUCK_TUCK002__Principles_of_Marketing',
+  'ASCL_ASCL010_01__Introduction_to_Chinese_Culture',
   'ANTH_ANTH050_34__Peoples_of_Oceania',  // Note: Adjust if the '/courses/' prefix is part of the ID in Firestore.
   'ANTH_ANTH006__Introduction_to_Biological_Anthropology',
   'MATH_MATH030_04__Evolutionary_Game_Theory_and_Applications',
@@ -10,6 +11,7 @@ export const hiddenLayupCourseIds = [  'PHIL_PHIL026__Philosophy_and_Computers',
   'MUS_MUS001__Beginning_Music_Theory' ,
   'SPEE_SPEE020__Public_Speaking',
   'HIST_HIST092_04__Partition_in_South_Asia',
+  'ENGL_ENGL055_01__Modern_American_Women_Poets',
   'SOCY_SOCY001__Introductory_Sociology',
   'MUS_MUS003_02__American_Music__Covers__Theft__and_Musical_Borrowing',
   'ENGL_ENGL015__Shakespeare',
